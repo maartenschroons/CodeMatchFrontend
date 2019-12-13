6f0539698e2639a3cff1ef000dfa9f7372cf01dc
maartenschroons <r0761272@student.thomasmore.be>
2019-12-13 18:06:42 +0100

    Commit

--- src/app/dashboard/company-dashboard/ongoing-assignments/ongoing-assignments.component.ts
+++ src/app/dashboard/company-dashboard/ongoing-assignments/ongoing-assignments.component.ts
@@ -4,6 +4,7 @@ import { Assignment } from 'src/app/models/assignment.model';
 import { Router } from '@angular/router';
 import { AppService } from 'src/app/services/app.service';
 import * as jwt_decode from 'jwt-decode';
+import { NotificationDto } from 'src/app/models/notification-dto.model';
 
 @Component({
   selector: 'app-ongoing-assignments',
@@ -16,16 +17,18 @@ export class OngoingAssignmentsComponent implements OnInit {
   assignmentsLength: number;
   decoded;
   companyID: number;
-  
+  userID: number;
+
   constructor(private router: Router, private _appService: AppService) {
     this.instantiateLists()
   }
   instantiateLists() {
     this.decoded = jwt_decode(localStorage.getItem("token"));
     this.companyID = this.decoded["CompanyID"];
+    this.userID = this.decoded["UserID"];
 
     this.assignments = this._appService.getAllInProgressAssignmentsByCompany(this.companyID);
-    this.assignments.subscribe(result => { this.assignmentsLength = result.length});
+    this.assignments.subscribe(result => { this.assignmentsLength = result.length });
   }
 
 
@@ -36,10 +39,18 @@ export class OngoingAssignmentsComponent implements OnInit {
     //console.log(assignment);
     this._appService.gekozenAssignment.next(assignment);
     this.router.navigate(["detailAssignment"]);
- }
+  }
 
- complete(a: Assignment){
-  a.status="Completed";
-  this._appService.editAssignment(a).subscribe(result =>{this.instantiateLists()});
-}
+  complete(a: Assignment) {
+    a.status = "Completed";
+    this._appService.editAssignment(a).subscribe(result => {
+      a.applications.forEach(application => {
+        let not = new NotificationDto(this.userID, application.makerID, a.assignmentID, 0, 0);
+        this._appService.PostNotification(not).subscribe();
+      }); 
+      this.instantiateLists()
+    });
+
+
+  }
 }
