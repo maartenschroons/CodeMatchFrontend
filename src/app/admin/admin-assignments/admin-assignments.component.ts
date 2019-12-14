0f0f0a998512990052107211639988ea28a9d6d1
maartenschroons <r0761272@student.thomasmore.be>
2019-12-14 12:03:41 +0100

    Commit

--- src/app/admin/admin-assignments/admin-assignments.component.ts
+++ src/app/admin/admin-assignments/admin-assignments.component.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Assignment } from 'src/app/models/assignment.model';
 import { AppService } from 'src/app/services/app.service';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-assignments',
@@ -13,7 +14,7 @@ export class AdminAssignmentsComponent implements OnInit {
   assignments: Observable<Assignment[]>;
   assignmentFilter: Observable<Assignment[]>;
 
-  constructor(private _appService: AppService) {
+  constructor(private _appService: AppService, private router: Router) {
     this.instantiateLists();
   }
 
@@ -33,5 +34,10 @@ export class AdminAssignmentsComponent implements OnInit {
         this.assignments = of(result);
       })
   }
+  bekijkAssignment(assignment: Assignment) {
+    //console.log(assignment);
+    this._appService.gekozenAssignment.next(assignment);
+    this.router.navigate(["detailAssignment"]);
+ }
 
 }
